642dbd8313525e14ddcef0cd829c7c17b413a971
gabrielblimoni <gabrielblimoni@gmail.com>
2020-08-11 00:29:37 -0300

    setting pipeline 12

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,6 +4,8 @@ mongoose.Promise = global.Promise
 const mongodbUri: string = String(process.env.MONGODB_URI)
 const mongodbDatabase: string = String(process.env.MONGODB_DATABASE)
 
+console.log('DATABASE:', mongodbUri, mongodbDatabase)
+
 export default {
     connect () {
         return mongoose.connect(mongodbUri, { 
