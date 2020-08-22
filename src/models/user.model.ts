918168ee0ecc723ff40c213643a65f074bc350cb
gabrielblimoni <gabrielblimoni@gmail.com>
2020-08-22 13:08:08 -0300

    adding refreshTokens data to User Model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,12 +1,15 @@
 import iUser from '@interfaces/user.interface'
 import { Document, Model, Schema, model } from 'mongoose'
 
-interface iUserDocument extends iUser, Document {}
+interface iUserDocument extends iUser, Document {
+    refreshTokens: String[],
+}
 interface iUserModel extends Model<iUserDocument> {}
 const UserSchema = new Schema<iUserDocument & iUserModel>({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    refreshTokens: [ String ],
 })
 
 const UserModel: iUserModel = model<iUserDocument, iUserModel>(
